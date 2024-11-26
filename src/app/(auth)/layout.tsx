8696dc4d450b572dc5f9f441d6b602e4a176a076
Fejiro Ogunje <fejiro.ogunje@seerglobalsolutions.com>
2024-11-26 16:05:37 +0100

    implemented authentication layout setup

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,32 @@
-// import styles from "./auth.module.scss";
+"use client"
+
+import { Column, Grid } from "@carbon/react"
+
+import Image from "next/image"
+
+import styles from "./auth.module.scss"
 
 const AuthLayout = ({
    children,
 }: Readonly<{
    children: React.ReactNode
 }>) => {
-   return <div>{children}</div>
+   return (
+      <Grid as="main" fullWidth className={styles.container}>
+         <Column lg={8} md={6} sm={12} className={styles.center}>
+            <div className={styles.children}>
+               <Image
+                  src="/svg/auth-header.svg"
+                  alt=""
+                  width={60}
+                  height={16}
+                  className={styles.authHeader}
+               />
+               {children}
+            </div>
+         </Column>
+      </Grid>
+   )
 }
 
 export default AuthLayout
