86e05dc8c5b7d8a9221b51b593421fb138f73f2e
Fejiro Ogunje <fejiro.ogunje@seerglobalsolutions.com>
2024-11-26 14:22:27 +0100

    added carbon icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
+import { Add } from "@carbon/icons-react";
 import { Button } from "@carbon/react";
 
 const Home = () => {
   return (
     <div>
+      <Add size="24" />
       <Button>Login</Button>
     </div>
   );
